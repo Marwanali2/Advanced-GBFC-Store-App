e5c732707805511635d35030815a9e4e88a25838
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-12-01 11:28:08 +0200

    first commit

---
+++ lib/adv_gbfc_store.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class AdvancedGBFCStore extends StatelessWidget {
+  const AdvancedGBFCStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Advanced GBFC Store'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Advanced GBFC Store!'),
+      ),
+    );
+  }
+}
